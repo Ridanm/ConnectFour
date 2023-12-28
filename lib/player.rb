b8a02cf6b248c05d2a40862ab9fa7d12a27de9a9
Ghost <danyfox1.dm@gmail.com>
2023-12-28 07:19:48 -0300

    Method piece created

--- lib/player.rb
+++ lib/player.rb
@@ -1,5 +1,17 @@
+require 'colorize'
 class Player 
-	def initalize 
+	attr_reader :name 
 
+	def initialize(name)
+		@name = name.capitalize 
+	end
+
+	def piece 
+		"\u3007"
 	end
 end
+
+player = Player.new('joe')
+p player.name 
+
+puts player.piece.blue
