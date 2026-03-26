6a5043a3cae7b9bcdabe4c08b1edab2a23173a04
Ridanm <ridanm@hotmail.com>
2026-03-26 06:26:30 -0300

    Added the methods #enter_piece_color, #entre_name

--- lib/player.rb
+++ lib/player.rb
@@ -1,11 +1,30 @@
-class Player 
-	attr_reader :name 
+# frozen_string_literal: true
 
-	def initialize(name)
-		@name = name.capitalize 
-	end
+require '../lib/dependencies'
+# This class creates the player with their name and the color of the piece to play.
+class Player < Board
+  attr_reader :name, :piece_color
 
-	def piece 
-		"\u2B55"
-	end
+  def initialize
+    @name = 'Player'
+  end
+
+  def enter_name
+    print 'Enter your name: '
+    name = gets.chomp.capitalize.strip.squeeze(' ')
+    if name == '' || name == ' '
+      @name
+    else
+      @name = name
+    end
+    @name
+  end
+
+  def enter_piece_color
+    valid_colors = %w(red blue yellow)
+    print "Select circle color (#{valid_colors.join(', ')}): "
+    color = gets.chomp.downcase
+    return piece(color) if valid_colors.include?(color)
+    enter_piece_color
+  end
 end
