553efda1df04b5d8e2283e54e7ca771af8cecbb4
Ridanm <ridanm@hotmail.com>
2026-03-31 07:50:42 -0300

    Created the #valid_column! and full_column? methods

--- lib/game.rb
+++ lib/game.rb
@@ -1,15 +1,36 @@
 # frozen_string_literal:true
 
+require_relative 'dependencies'
+
 # This class implements column validation, player creation, and how the pieces are arranged on the board.
 class Game
-  def initialize; end
+  attr_reader :board
 
-  def valid_move?(column); end
+  include Info
 
-  def player_move; end
+  def initialize(board)
+    @board = board
+  end
 
-  def drop_piece(column, symbol)
-    
+  def valid_column!
+    column = gets.chomp.to_i
+    return column if column.between?(1, 7
+
+    puts Info.message('select column')
+    valid_column!
+  end
+
+  def full_column?(column)
+    if board.boxes[column - 1][0] != ' '
+      puts Info.message('full column')
+      return true
+    end
+    false
+  end
+
+  def player_move(piece)
+    column = valid_column
+    board.drop_piece(column, piece)
   end
 
   def switch_player; end
