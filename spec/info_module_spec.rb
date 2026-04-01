149fa32495bea12e9772d169b8cdaedab65703a8
Ridanm <ridanm@hotmail.com>
2026-04-01 06:54:54 -0300

    tests of constants

---
+++ spec/info_module_spec.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require 'colorize'
+require '../lib/info_module'
+
+RSpec.describe 'Info' do
+  describe 'constants' do
+    msj = ' Welcome to Connect Four game '
+    it 'we call the header' do
+      expect(Info::HEADING).to eq(msj)
+    end
+
+    it 'when we call the a space constant' do
+      expect(Info::A_SPACE).to eq(' ')
+    end
+
+    it 'when we call the three spaces constant' do
+      expect(Info::THREE_SPACES).to eq('   ')
+    end
+  end
+
+  describe '#message' do
+    msj = 'Select a column between 1 and 7.'
+    it 'when the row number is incorrect' do
+      expect(Info.message('select column')).to eq(msj)
+    end
+
+    it 'The column is full' do
+      msj = 'This column is complete.'.light_red
+      expect(Info.message('full column')).to eq(msj)
+    end
+  end
+end
