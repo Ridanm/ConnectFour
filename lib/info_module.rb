340d2f7d1608791eb50e72b936e594a21a8da6bd
Ridanm <ridanm@hotmail.com>
2026-04-12 08:32:10 -0300

    Welcome message added

--- lib/info_module.rb
+++ lib/info_module.rb
@@ -7,7 +7,8 @@ module Info
   THREE_SPACES = '   '
 
   def self.message(parameter)
-    { 'select column' => 'Select a column between 1 and 7.',
+    { 'welcome' => 'welcome to connect four'.upcase.on_blue,
+      'select column' => 'Select a column between 1 and 7.',
       'full column' => 'This column is complete.'.light_red
     }[parameter]
   end
