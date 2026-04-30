909a26b96476fe09756de53163132a9fda945ff2
Ridanm <ridanm@hotmail.com>
2026-04-30 06:48:46 -0300

    #play_turn refactored

--- lib/game.rb
+++ lib/game.rb
@@ -4,7 +4,7 @@ require_relative 'dependencies'
 
 # This class implements column validation, player creation, and how the pieces are arranged on the board.
 class Game
-  attr_reader :board, :player, :player_one, :player_two, :number_of_moves
+  attr_accessor :board, :player, :player_one, :player_two, :number_of_moves
 
   include Info
 
@@ -17,7 +17,7 @@ class Game
   end
 
   def presentation
-    puts Info.message('welcome')    end
+    puts Info.message('welcome')      end
 
   def full_column?(column)              if board.boxes[column - 1][0] != ' ' 
       puts Info.message('full column
@@ -32,18 +32,19 @@ class Game
     player.odd? ? player_one : player_two
   end
 
-  def player_move(column, piece)
-    board.drop_piece?(column, piece)
-  end
-
   def play_turn
     board.show_board
-    # piece = player.enter_piece_collor # Remove selected color to avoid repetition
     column = player.valid_column
-    until player_move?(column, player.piece)
+    until board.drop_piece?(column, player.piece_color)
+      full_column?(column)
       column = player.valid_column
     end
-    number_of_moves += 1
+
+    increase_box_number
+  end
+
+  def increase_box_number
+    @number_of_moves += 1
   end
 
   def win?; end
