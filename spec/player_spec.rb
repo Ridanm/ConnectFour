31effb52f7b3269b92a6a90aee5de9a95c9c9dcd
Ridanm <ridanm@hotmail.com>
2026-04-30 06:58:37 -0300

    #select_piece_color refactored

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,59 +2,87 @@
 
 require '../lib/dependencies'
 
-RSpec.describe Player do
+RSpec.describe 'Player' do
+  let(:piece) { "\u2742" }
+  let(:red_piece) { piece.red }
+  let(:player_one) { Player.new('Joe', 'red') }
+  let(:player_two) { Player.new('blue') }
+
   describe '#initialize' do
     it 'when an instance of the class is created' do
-      expect(subject).to be_an_instance_of(Player)
+      expect(player_one).to be_an_instance_of(Player)
     end
 
     it 'before entering the name' do
-      expect(subject.name).to be('Player')
+      expect(player_two.name).to be('Player')
+    end
+
+    it 'when creating the player, the color of the piece' do
+      player_three = Player.new('Ben', 'yellow')
+      expect(player_three.piece_color).to eq("\u2742".yellow)
     end
   end
 
   describe '#enter_name' do
     it 'when the player enters their name' do
-      expect(subject).to receive(:gets).and_return('Joe')
-      subject.enter_name
-      expect(subject.name).to eq('Joe')
+      expect(player_one).to receive(:enter_name).and_return('Joe')
+      player_one.enter_name
+      expect(player_one.name).to eq('Joe')
     end
 
     it 'when the player does not enter their name' do
-      expect(subject).to receive(:gets).and_return(' ')
-      subject.enter_name
-      expect(subject.name).to eq('Player')
+      expect(player_two).to receive(:enter_name).and_return(' ')
+      player_two.enter_name
+      expect(player_two.name).to eq('Player')
     end
   end
 
-  describe '#enter_piece_color' do
-    it 'when you select the red piece' do
-      expect(subject).to receive(:gets).and_return('red')
-      expect(subject.enter_piece_color).to eq("\u2742".red)
+  describe '#select_piece_color' do
+    it 'when you select the blue piece' do
+      expect(player_one).to receive(:gets).and_return("blue\n")
+      expect(player_one.select_piece_color).to eq('blue')
     end
 
-    it 'when select yellow piece' do
-      expect(subject).to receive(:gets).and_return('yellow')
-      expect(subject.enter_piece_color).to eq("\u2742".yellow)
+    it 'when select cyan piece' do
+      expect(player_one).to receive(:gets).and_return("cyan\n")
+      expect(player_one.select_piece_color).to eq('cyan')
     end
 
     it 'when you select a color that is not' do
-      expect(subject).to receive(:gets).and_return('black', 'blue')
-      expect(subject).to receive(:enter_piece_color).and_call_original.exactly(2).times
-      expect(subject.enter_piece_color).to eq("\u2742".blue)
+      expect(player_two).to receive(:gets).and_return("black\n", "yellow\n")
+      expect(player_two).to receive(:select_piece_color).and_call_original.exactly(2).times
+      expect(player_two.select_piece_color).to eq('yellow')
+    end
+  end
+
+  describe '#piece(color)' do
+    let(:color_white) { player_one.piece('white') }
+    let(:color_blue) { player_two.piece('blue') }
+    let(:color_yellow) { player_one.piece('yellow') }
+
+    it 'color is white' do
+      expect(color_white).to eq(piece.white)
+    end
+
+    it 'color is blue' do
+      expect(color_blue).to eq(piece.blue)
+    end
+
+    it 'color is yellow' do
+      expect(color_yellow).to eq(piece.yellow)
     end
   end
 
   describe '#valid_column' do
     it 'when the column number is correct' do
-      expect(subject).to receive(:gets).and_return("4\n")
-      expect(subject.valid_column).to eq(4)
+      expect(player_one).to receive(:valid_column).and_return(4)
+      expect(player_one.valid_column).to eq(4)
     end
 
     it 'when the column number is not found' do
-      expect(subject).to receive(:gets).and_return("8\n", "2\n")
-      expect(subject).to receive(:valid_column).and_call_original.exactly(2).times
-      expect(subject.valid_column).to eq(2)
+      expect(player_two).to receive(:gets).and_return("8\n", "2\n")
+      expect(player_two).to receive(:valid_column).and_call_original.exactly(2).times
+      expect(player_two.valid_column).to eq(2)
     end
   end
 end
