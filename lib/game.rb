63c120d49d08948bb6ebb0087951cb75a0411369
Ridanm <ridanm@hotmail.com>
2026-05-02 08:42:49 -0300

    Added vkctory methods

--- lib/game.rb
+++ lib/game.rb
@@ -19,12 +19,10 @@ class Game
   def presentation
     puts Info.message('welcome')      end
 
-  def full_column?(column)              if board.boxes[column - 1][0] != ' ' 
-      puts Info.message('full column
-')
+  def full_column?(column)              if board.boxes[0][column - 1] != A_SPACE
       return true
     end
-    false
+      false
   end
 
   def swap_player
@@ -36,18 +34,66 @@ class Game
     board.show_board
     column = player.valid_column
     until board.drop_piece?(column, player.piece_color)
-      full_column?(column)
+      puts Info.message('full column')
       column = player.valid_column
+
     end
 
-    increase_box_number
-  end
-
-  def increase_box_number
     @number_of_moves += 1
   end
 
-  def win?; end
+  def horizontal_victory?
+    @board.boxes.each do |row|
+      row.each_cons(4) do |line|
+        return true if line.all? { |box| box == player.piece_color }
+      end
+    end
+
+    false
+  end
+
+  def vertical_victory?
+    transpose_boxes = @board.boxes.transpose
+    transpose_boxes.each do |row|
+      row.each_cons(4) do |column|
+        return true if column.all? { |box| box == player.piece_color }
+      end
+    end
+
+    false
+  end
+
+  def descending_diagonal?
+    (0..2).each do |row|
+      (0..3).each do |col|
+        return true if (0..3).all? { |ind| board.boxes[row + ind][col + ind] == player.piece_color}
+      end
+    end
+
+    false
+  end
+
+  def ascending_diadonal?
+    (3..5).each do |row|
+      (0..3).each do |col|
+        return true if (0..3).all? { |ind| board.boxes[row - ind][col + ind] == player.piece_color }
+      end
+    end
+
+    false
+  end
+
+  def diagonal_victory?
+    descending_diagonal? || ascending_diadonal?
+  end
+
+  def winner?
+    horizontal_victofy? || vertical_victory? || diagonal_victory?
+  end
+
+  def draw
+    puts Info.message("It's a draw") if number_of_moves > 42 and winner? == false
+  end
 
   def game_over?; end
 
