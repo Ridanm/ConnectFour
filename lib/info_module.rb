a10f8de7123d6ddb14bc513a4f400730cb14d531
Ridanm <ridanm@hotmail.com>
2026-05-02 08:44:54 -0300

    #message(it's a draw) added

--- lib/info_module.rb
+++ lib/info_module.rb
@@ -9,8 +9,9 @@ module Info
   def self.message(parameter)
     { 'welcome' => 'welcome to connect four'.upcase.blue,
       'select column' => 'Select a column between 1 and 7: ',
-      'full column' => 'This column is complete, please select another !!!'.light_red,
-      'select color' => 'Select the color of the piece you will play with: '
+      'full column' => 'This column is complete, please select another !!!'.yellow,
+      'select color' => 'Select the color of the piece you will play with: ',
+      "it's a draw" => 'The board is full and there is no winner, they want to play again.'
     }[parameter]
   end
 end
