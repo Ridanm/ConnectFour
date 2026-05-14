80076628f9bb5516df02fb7799506b47e7704af3
Ridanm <ridanm@hotmail.com>
2026-05-14 08:03:57 -0300

    #enter_name and #select_piece_color moved to info_module

--- lib/player.rb
+++ lib/player.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
-require '../lib/dependencies'
+require_relative 'board'
+require_relative 'info_module'
 
 # This class creates the player with their name and the color of the piece to play.
 class Player < Board
@@ -10,38 +11,17 @@ class Player < Board
   include Info
 
   def initialize(name = 'Player', color)
-    @piece = "\u2742"
     @name = name
     @piece_color = piece(color)
   end
 
-  def enter_name
-    print 'Enter your name: '
-    write_name = gets.chomp.capitalize.strip.squeeze(' ')
-    if ['', ' '].include?(write_name)
-      @name
-    else
-      @name = write_name
-    end
-  end
-
-  def select_piece_color
-    valid_colors = %w[ blue cyan red white yellow ]
-    print "#{Info.message('Select color')} #{valid_colors.join(', ')}: "
-    color = gets.chomp.downcase
-    return color if valid_colors.include?(color)
-
-    select_piece_color
-  end
-
   def piece(color)
-    { 'white' => "\u2742".white,
-      'red' => "\u2742".red,
-      'yellow' => "\u2742".yellow,
-      'blue' => "\u2742".blue,
-      'light red' => "\u2742".light_red,
-      'cyan' => "\u2742".cyan,
-      'light blue' => "\u2742".light_blue }[color]
+    { '1' => "\u2742".red,
+      '2' => "\u2742".yellow,
+      '3' => "\u2742".blue,
+      '4' => "\u2742".light_red,
+      '5' => "\u2742".cyan,
+      '6' => "\u2742".light_blue }[color]
   end
 
   def valid_column
