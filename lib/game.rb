d86fa9ae4395f518d75074fd977b4cf5b262dccf
Ridanm <ridanm@hotmail.com>
2026-05-14 08:02:03 -0300

    #play_turn added #swap_player #play modified

--- lib/game.rb
+++ lib/game.rb
@@ -17,9 +17,6 @@ class Game
     @player = swap_player
   end
 
-  def presentation
-    puts Info.message('welcome')      end
-
   def full_column?(column)              if board.boxes[0][column - 1] != A_SPACE
       return true
     end
@@ -27,24 +24,24 @@ class Game
   end
 
   def swap_player
-    player = number_of_moves
-    player.odd? ? player_one : player_two
+    @player = (@number_of_moves.odd?) ? @player_one : @player_two
   end
 
   def play_turn
     board.show_board
-    column = player.valid_column
-    until board.drop_piece?(column, player.piece_color)
+    column = @player.valid_column
+    until board.drop_piece?(column, @player.piece_color)
       puts Info.message('full column')
-      column = player.valid_column
+      column = @player.valid_column
     end
     @number_of_moves += 1
+    swap_player
   end
 
   def winner?
     all_lines.any? do |line|
       line.each_cons(4).any? do |cons|
-        cons.all?(player.piece_color)
+        cons.all?(@player.piece_color)
       end
     end
   end
@@ -54,8 +51,7 @@ class Game
   end
 
   def play
-    presentation
-    until full_board? || winner?
+    until winner? || full_board?
       play_turn
       puts Info.message('congratulations') if winner?
       puts Info.message("it's a draw") if draw
