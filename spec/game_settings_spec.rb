d8594393653626b1c243ec31a5095c5b2c99f991
Ridanm <ridanm@hotmail.com>
2026-05-20 20:25:21 -0300

    test of methods a, #create_player and #before_staring  completed

--- spec/game_settings_spec.rb
+++ spec/game_settings_spec.rb
@@ -6,10 +6,12 @@ require '../lib/game_settings'
 # The GameSettings class is responsible for obtaining the data necessary for creating the players.
 RSpec.describe GameSettings do
   let(:piece) { "\u2742" }
+  let(:setup) { GameSettings.new }
 
   describe '#initialize' do
     it 'when initialized with the info module colors' do
       expect(subject.avaliable_colors).not_to be_empty
+      expect(subject.avaliable_colors).to eq(Info::COLORS.dup)
     end
 
     it "Let's check the available colors" do
@@ -25,8 +27,10 @@ RSpec.describe GameSettings do
   describe '#create_player' do
     before do
       allow(Info).to receive(:enter_name).and_return('Alice')
-      allow(subject).to receive(:show_colors)
+      expect(subject).to receive(:show_colors)
       allow(subject).to receive(:gets).and_return("1\n")
+      allow(subject).to receive(:print)
+      allow(subject).to receive(:puts)
       allow(Info).to receive(:select_piece_color).and_return("1\n")
     end
 
@@ -39,12 +43,38 @@ RSpec.describe GameSettings do
   end
 
   describe '#before_starting' do
-    xit "when it's a single player" do
-      allow(subject).to receive(:gets).and_return("1\n", 'joe', "3\n")
-      #allow(subject).to receive(:create_player).and_return("joe\n")
-      #allow(subject).to receive(:create_player).and_return("3\n")
-      expect(subject.player_one).to eq(Player.new('joe', '3'))
-      subject.create_player
+    before do
+      allow(subject).to receive(:presentation)
+      allow(subject).to receive(:puts)
+    end
+
+    context 'if the number of players is 1' do
+      it 'when a player is configured' do
+        allow(subject).to receive(:number_of_players).and_return('1')
+        fake_player = Player.new('Julia', '2')
+        allow(subject).to receive(:create_player).and_return(fake_player)
+
+        subject.before_starting
+
+        expect(subject.player_one).to eq(fake_player)
+        expect(subject.player_one.name).to eq('Julia')
+        expect(subject.player_one.piece_color).to eq(piece.yellow)
+        expect(subject.player_two).to be_nil
+      end
+    end
+
+    context 'if the number of players is 2' do
+      it 'when two players are configured' do
+        allow(subject).to receive(:number_of_players).and_return('2')
+        player1 = Player.new('Anton', '1')
+        player2 = Player.new('Brian', '5')
+        allow(subject).to receive(:create_player).and_return(player1, player2)
+
+        subject.before_starting
+
+        expect(subject.player_one).to eq(player1)
+        expect(subject.player_two).to eq(player2)
+      end
     end
   end
 end
