4d3947f864f61481d2de162b66a721b97de18e35
Ridanm <ridanm@hotmail.com>
2026-05-25 08:47:49 -0300

    Virtual player tests

---
+++ spec/computer_player_spec.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require_relative '../lib/player'
+require_relative '../lib/computer_player'
+
+RSpec.describe VirtualPlayer do
+  let(:computer) { Player.new('Ap', '2')}
+  let(:piece) { "\u2742" }
+
+  describe '#initialize' do
+    comp = VirtualPlayer.new('1')
+
+    it 'the name of the virtual player' do
+      expect(comp.name).to eq('Computer')
+    end
+
+    it 'the color of the virtual player' do
+      expect(comp.piece_color).to eq(piece.red)
+    end
+  end
+
+  describe '#valid_column' do
+    it 'when column 2 is selected' do
+      allow(computer).to receive(:valid_column).and_return(2)
+      expect(computer.valid_column).to eq(2)
+    end
+  end
+end
