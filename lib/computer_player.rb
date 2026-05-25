29a4860feac8c2cca998cb807e3464b40c8938fb
Ridanm <ridanm@hotmail.com>
2026-05-25 08:46:18 -0300

    We begin with a virtual player class

---
+++ lib/computer_player.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require_relative '../lib/dependencies'
+
+# This class implements the autonomous player in case of playing against the computer
+class VirtualPlayer < Player
+  attr_reader :name, :piece_color
+
+  def initialize(color)
+    super('Computer', color)
+    # @piece_color = piece(color)
+  end
+
+  def make_move
+
+  end
+end
