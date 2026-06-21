ffb0c0ef8644e751d9b5629782657aea559882ce
Ridanm <ridanm@hotmail.com>
2026-06-21 20:35:27 -0300

    description of the game execution

--- lib/main.rb
+++ lib/main.rb
@@ -2,20 +2,21 @@
 
 require_relative 'dependencies'
 
-# Comienzo del.juego 
-# 1- Mensaje de bienvenida
-#    - mostrar colores disponibles
-# 2- Son uno o dos jugadores ?
-#    - ingresa el nombre del jugador
-#    - color de la pieza con que juega
-#    - quitar el color seleccionado para el proximo jugador 
-#    - implementar jugador autonomo
-# 3- Dos jugadore 
-#    - primer jugador nombre y color de la pieza con la que jugara
-#    - segundo jugador repite paso anterior
-# 4- Creamos la clase Tablero
-# 5- Creamos la clase Game con sus argumentos
-# 6- Ejecutamos el método
+# Start the Game 
+# 1- Welcome message
+# 2- Select 
+#    - Enter player name
+#    - Select piece color
+#      - Delete the selected color
+#    - Implement bot
+# 3- Two players
+#    - First player name and select color of the piece
+#    - Second player repeats previous steps
+# 4- Main
+#    - GameSettings instance
+#    - GameSettings.before_starting complete the player setup steps
+# 5- Game instance
+#    - execution of the play method
 
 game_settings = GameSettings.new
 game_settings.before_starting
