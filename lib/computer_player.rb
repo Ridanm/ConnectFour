2145ec50d8830f0581dac7bdede3805bf8578817
Ridanm <ridanm@hotmail.com>
2026-06-21 11:04:12 -0300

    Method for adding empty columns and columns with free spaces

--- lib/computer_player.rb
+++ lib/computer_player.rb
@@ -1,18 +1,63 @@
 # frozen_string_literal: true
 
-require_relative '../lib/dependencies'
-
+require_relative 'dependencies'
 # This class implements the autonomous player in case of playing against the computer
-class VirtualPlayer < Player
-  attr_reader :name, :piece_color
+class VirtualPlayer
+  attr_reader :name, :piece_color, :color
+  
+  include Info
 
   def initialize(color)
-    super('Computer', color)
-    # @piece_color = piece(color)
+    @name = 'Alpha_4'
+    @piece_color = piece(color)
   end
 
   def valid_column
     print 'Is thimking...'
+    puts "\n#{name} is calculating best move... 🤖".blue
     sleep(1)
+    avaliable_free_columns
+    column_with_empty_cells if avaliable_free_columns.empty?
+  
+  # Prioridad 1: ¿Puedo ganar en este turno?
+ # winning_move = board.find_winning_move(color)
+  #return winning_move if winning_move
+
+  # Prioridad 2: ¿El rival va a ganar en su próximo turno? ¡Bloquear!
+ # opponent_color = (color == 'red' ? 'yellow' : 'red') # Ajusta según tus colores
+  #blocking_move = board.find_winning_move(opponent_color)
+  #return blocking_move if blocking_move
+
+  # Prioridad 3: Si no hay peligro ni victoria inminente, elegir al azar o el centro
+  #board.free_columns.sample
+   # sleep(1)
+  end
+
+  def find_winning_move(alpha_color)
+    # verificar que el color se pueda colocar en linea, columna, diagonal
+    # de lo contrario elegir otra columna donde tenga mas fichas si las hay
+  end
+
+  def opponent_winning_move(oponnent_color)
+    
+  end
+
+  def avaliable_free_columns(board = @board)
+    columns = []
+    free_columns = board.boxes.transpose
+    free_columns.each_with_index do |row, index|
+      columns << index + 1 if row.all? { |element| element == ' ' }
+    end
+    #.implement step when columns are empty
+      columns.sample
+  end
+
+  def column_with_empty_cells(board = @board)
+    columns = []
+    squqres_with_space = board.boxes.transpose
+    squqres_with_space.each_with_index do |row, index|
+      columns << index + 1 if row.any? { |element| element == ' ' }
+    end
+      columns.sample
   end
 end
