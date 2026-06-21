cbd275b2abb91a2cbf0eddbe90dcc5623b8a1a48
Ridanm <ridanm@hotmail.com>
2026-06-21 11:11:08 -0300

    Tests of the free column method, columns with free spaces

--- spec/computer_player_spec.rb
+++ spec/computer_player_spec.rb
@@ -1,27 +1,57 @@
 # frozen_string_literal: true
 
-require_relative '../lib/player'
-require_relative '../lib/computer_player'
+require_relative '../lib/dependencies'
 
 RSpec.describe VirtualPlayer do
-  let(:computer) { Player.new('2')}
   let(:piece) { "\u2742" }
-
+  let(:board) { Board.new }
+  bot = VirtualPlayer.new('1')
+  
   describe '#initialize' do
-    comp = VirtualPlayer.new('1')
-
     it 'the name of the virtual player' do
-      expect(comp.name).to eq('Alpha_4')
+      expect(bot.name).to eq('Alpha_4')
     end
 
     it 'the color of the virtual player' do
-      expect(comp.piece_color).to eq(piece.red)
+      expect(bot.piece_color).to eq(piece.red)
     end
   end
 
-  describe '#valid_column' do
-    it 'when column 2 is selected' do
-      expect(computer.valid_column).to eq(2)
+  describe '#avaliable_free_columns(board)' do
+    context 'when there are free columns' do
+      before do
+        6.times { |column_number| board.drop_piece?(column_number + 1, piece) }
+      end
+      
+      it 'the bot can return the column number' do
+        expect(bot.avaliable_free_columns(board)).to eq(7)
+      end
+    end
+
+    context 'when there are no free columns' do
+      before do
+        7.times { |column_number| board.drop_piece?(column_number + 1, piece) }
+      end
+
+      it 'the bot return any box between 1 and 7' do
+        allow(bot).to receive(:avaliable_free_columns).with(board).and_return(2)
+        expect(bot.avaliable_free_columns(board)).to eq(2)  
+      end
+    end
+  end
+
+  describe '#column_with_empty_cells(board)' do
+    let(:fake_board) { double("Board") }
+
+    before do
+      allow(bot).to receive(:column_with_empty_cells).with(fake_board).and_return([1, 2, 5, 7])
+    end
+    
+    it 'if there are still empty spaces' do
+      result = bot.column_with_empty_cells(fake_board)
+      expect(result).to include(5)
+      expect(result).to include(2)
+      expect(result).not_to include(4)
     end
   end
 end
