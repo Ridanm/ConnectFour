7560804a07b01bacb69dfd7b7be28a2d4e2d1d5d
Ridanm <ridanm@hotmail.com>
2026-06-22 14:20:40 -0300

    Test for #valid_column(board) modified

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe 'Player' do
   let(:red_piece) { piece.red }
   let(:player_one) { Player.new('Joe', '1') }
   let(:player_two) { Player.new('2') }
+  let(:board) { Board.new }
 
   describe '#initialize' do
     it 'when an instance of the class is created' do
@@ -37,16 +38,16 @@ RSpec.describe 'Player' do
     end
   end
 
-  describe '#valid_column' do
+  describe '#valid_column(board)' do
     it 'when the column number is correct' do
-      allow(player_one).to receive(:valid_column).and_return(4)
-      expect(player_one.valid_column).to eq(4)
+      allow(player_one).to receive(:valid_column).with(board).and_return(4)
+      expect(player_one.valid_column(board)).to eq(4)
     end
 
     it 'when the column number is not found' do
       allow(player_two).to receive(:gets).and_return("8\n", "2\n")
-      expect(player_two).to receive(:valid_column).and_call_original.exactly(2).times
-      expect(player_two.valid_column).to eq(2)
+      expect(player_two).to receive(:valid_column).with(board).and_call_original.exactly(2).times
+      expect(player_two.valid_column(board)).to eq(2)
     end
   end
 end
