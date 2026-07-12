47b0e369bd1cbce85882368f35394fa85b2ed39e
Ridanm <ridanm@hotmail.com>
2026-07-12 10:00:33 -0300

    Clean console added to the game

--- lib/main.rb
+++ lib/main.rb
@@ -2,22 +2,6 @@
 
 require_relative 'dependencies'
 
-# Start the Game 
-# 1- Welcome message
-# 2- Select 
-#    - Enter player name
-#    - Select piece color
-#      - Delete the selected color
-#    - Implement bot
-# 3- Two players
-#    - First player name and select color of the piece
-#    - Second player repeats previous steps
-# 4- Main
-#    - GameSettings instance
-#    - GameSettings.before_starting complete the player setup steps
-# 5- Game instance
-#    - execution of the play method
-
 system('clear')
 game_settings = GameSettings.new
 game_settings.before_starting
